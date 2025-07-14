a571d5936b7637dd61e6b4f6363e977b6e67ae5d
Souvik Sen <souviksen@CKMAC260.local>
2025-07-14 11:51:55 +0530

    initial commit

---
+++ force-app/main/default/lwc/list/list.js
@@ -0,0 +1,13 @@
+import { LightningElement } from 'lwc';
+import { bikes } from 'c/data';
+
+export default class List extends LightningElement {
+    bikes = bikes;
+
+    handleTileClick(evt) {
+        const event = new CustomEvent('productselected', {
+            detail: evt.detail
+        });
+        this.dispatchEvent(event);
+    }
+}
